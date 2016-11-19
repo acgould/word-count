287f62a009d01b46dc783bed2a33e59a1ff035b3
A C Gould <gould.ashleyc@gmail.com>
2016-11-19 15:30:19 -0800

    Reformatted testing file

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 from wordcount import WordCount
 
+def list_of_strings(length):
+    """This function creates a list of strings to aid testing,"""
+    list = range(length)
+    for i in range(length):
+        list[i] = str(i)
+    return list
+
 def test_file_to_words(file):
     """
     This tests that the file properly opens and returns a list of the words
@@ -8,11 +15,32 @@ def test_file_to_words(file):
     wc = WordCount()
     return wc._file_reader(file)
 
+def all_test_file_to_words():
+    print "Testing ability to open file and return words: "
+    assert test_file_to_words('blank.txt') == []
+    print test_file_to_words('words.txt')
+    print test_file_to_words('wordswithcaps.txt')
+    print test_file_to_words('wordswithpunct.txt')
+    print test_file_to_words('wordswithnewlines.txt')
+
 def test_clean_words(list):
     wc = WordCount()
     for i in range(len(list)):
         list[i] = wc._word_cleaner(list[i])
     return list
+
+def all_test_clean_words():
+    print "Testing ability to clean words."
+    clean_list = ['a', 'b', 'c', 'd']
+    assert test_clean_words([]) == []
+    assert test_clean_words(['a', 'b', 'c', 'd']) == clean_list
+    assert test_clean_words(['A', 'B', 'C', 'D']) == clean_list
+    assert test_clean_words(['a.', 'b,', 'c?', 'd!']) == clean_list
+    assert test_clean_words(['"a', 'b"', '"(c', 'd!"']) == clean_list
+    assert test_clean_words(['$%^^&*a', '!!!b!!!', 'c)))', 'd-']) \
+            == clean_list
+    assert test_clean_words(['Hello!']) == ['hello']
+    assert test_clean_words(["Don't!!"]) == ["don't"]
 
 def test_reset(num, list_of_words):
     wc1 = WordCount()
@@ -38,6 +66,12 @@ def test_reset(num, list_of_words):
     assert len(wc1._words_seen.items()) == len(wc2._words_seen.items())
     assert len(wc1._words_seen.items()) == 0
 
+def all_test_reset():
+    print "Testing proper reset."
+    test_reset(1, ['hi'])
+    test_reset(4, ['a', 'b', 'c', 'd'])
+    test_reset(500, list_of_strings(1000))
+
 def test_add(list_of_words):
     wc = WordCount()
     manual_count = 0
@@ -46,6 +80,13 @@ def test_add(list_of_words):
         manual_count += 1
         assert wc._words_seen[word] > 0
         assert wc._word_counter == manual_count
+
+def all_test_add():
+    print "Testing addition of words to increase counts."
+    test_add([])
+    test_add(['a', 'b', 'c', 'd'])
+    test_add(list_of_strings(100))
+    test_add(list_of_strings(100) + list_of_strings(200))
 
 def test_count_file(filename):
     """ This test only works for my numeric-based test files. """
@@ -57,6 +98,13 @@ def test_count_file(filename):
     assert wc._words_seen['two'] == 2
     assert wc._words_seen['three'] == 3
     assert wc._words_seen['four'] == 4
+
+def all_test_count_file():
+    "Testing addition of files to increase counts."
+    test_count_file('words.txt')
+    test_count_file('wordswithcaps.txt')
+    test_count_file('wordswithpunct.txt')
+    test_count_file('wordswithnewlines.txt')   
 
 def test_count_mult_files(list_of_filenames):
     wc = WordCount()
@@ -71,7 +119,17 @@ def test_count_mult_files(list_of_filenames):
         assert wc._words_seen['four'] == 4*mult
         mult += 1
 
-def test_word_count(list_of_words):
+def all_test_count_mult_files():
+    print "Testing addition of multiple files to up counts w/o reset."
+    all_files = ['words.txt', 'wordswithcaps.txt', 'wordswithpunct.txt', \
+        'wordswithnewlines.txt']
+    test_count_mult_files(all_files[:1])
+    test_count_mult_files(all_files[:2])
+    test_count_mult_files(all_files[:3])
+    test_count_mult_files(all_files[:4])
+    test_count_mult_files(all_files)    
+
+def test_word_count_total(list_of_words):
     wc = WordCount()
     manual_count = 0
     for word in list_of_words:
@@ -80,76 +138,36 @@ def test_word_count(list_of_words):
         assert wc.word_count_total() == manual_count
         assert wc.word_count_total() == wc._word_counter
 
-def list_of_strings(length):
-    list = range(length)
-    for i in range(length):
-        list[i] = str(i)
-    return list
+def all_test_word_count_total():
+    print "Testing proper return of total word count."
+    test_word_count_total([])
+    test_word_count_total(list_of_strings(100))
+    test_word_count_total(list_of_strings(1000))
+    test_word_count_total(list_of_strings(1000) + list_of_strings(1000))
 
 
 def run_tests():
-    file_to_words = False
-    clean_words = False
-    reset = False
-    adding_words = False
-    count_file = False
-    count_mult_files = True
-    return_count = True
+    # Test file to list of words
+    #all_test_file_to_words()
 
-    if file_to_words: 
-        print "Testing ability to open file and return words: "
-        assert test_file_to_words('blank.txt') == []
-        print test_file_to_words('words.txt')
-        print test_file_to_words('wordswithcaps.txt')
-        print test_file_to_words('wordswithpunct.txt')
-        print test_file_to_words('wordswithnewlines.txt')
+    # Test cleaning of words
+    all_test_clean_words()
 
-    if clean_words:
-        print "Testing ability to clean words."
-        clean_list = ['a', 'b', 'c', 'd']
-        assert test_clean_words([]) == []
-        assert test_clean_words(['a', 'b', 'c', 'd']) == clean_list
-        assert test_clean_words(['A', 'B', 'C', 'D']) == clean_list
-        assert test_clean_words(['a.', 'b,', 'c?', 'd!']) == clean_list
-        assert test_clean_words(['"a', 'b"', '"(c', 'd!"']) == clean_list
-        assert test_clean_words(['$%^^&*a', '!!!b!!!', 'c)))', 'd-']) \
-                == clean_list
-        assert test_clean_words(['Hello!']) == ['hello']
-        assert test_clean_words(["Don't!!"]) == ["don't"]
+    # Test resetting of counters
+    all_test_reset()
 
-    if reset:
-        print "Testing proper reset."
-        test_reset(1, ['hi'])
-        test_reset(4, ['a', 'b', 'c', 'd'])
+    # Test incrementing of counters
+    all_test_add()
 
-    if adding_words:
-        print "Testing addition of words to increase counts."
-        test_add([])
-        test_add(['a', 'b', 'c', 'd'])
+    # Test counting of words in a file
+    all_test_count_file()
 
-    if count_file:
-        "Testing addition of files to increase counts."
-        test_count_file('words.txt')
-        test_count_file('wordswithcaps.txt')
-        test_count_file('wordswithpunct.txt')
-        test_count_file('wordswithnewlines.txt')
-    
-    if count_mult_files:
-        print "Testing addition of multiple files to up counts w/o reset."
-        all_files = ['words.txt', 'wordswithcaps.txt', 'wordswithpunct.txt', \
-            'wordswithnewlines.txt']
-        test_count_mult_files(all_files[:1])
-        test_count_mult_files(all_files[:2])
-        test_count_mult_files(all_files[:3])
-        test_count_mult_files(all_files[:4])
-        test_count_mult_files(all_files)
+    # Test counting of words in multiple files
+    all_test_count_mult_files()
 
-    if return_count:
-        print "Testing proper return of total word count."
-        test_word_count([])
-        test_word_count(list_of_strings(100))
-        test_word_count(list_of_strings(1000))
-        test_word_count(list_of_strings(1000) + list_of_strings(1000))
-        
+    # Test that total word count is returned correctly
+    all_test_word_count_total()
+
+
 
 run_tests()
